9a8e35d7764f40521d6faca13e5ccf6bdccd0a19
Kia <82962111+KiaWeb@users.noreply.github.com>
2022-08-14 14:45:17 -0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -2,7 +2,7 @@
 Official Expo Tools Loader
 Expo in Expo Tools stands for Exploiter.
 Made by Kia.
-Alpha Test 0.0.0a.
+Alpha Test 0.1.1a.
 --]]
 getfenv().new = function(type,parent)
   return Instance.new(type,parent)
