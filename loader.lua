65cd9562de6041f911f8425949f557663941cee4
Kia <82962111+KiaWeb@users.noreply.github.com>
2022-08-14 14:43:13 -0500

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,8 @@
+--[[
+Official Expo Tools Loader
+Expo in Expo Tools stands for Exploiter.
+Made by Kia.
+Alpha Test 0.0.0a.
+--]]
+getfenv().new = function(type,parent)
+  return Instance.new(type,parent)
