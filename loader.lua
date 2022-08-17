5b422b826c5ffa75fb36ec7c693a8476ad25e224
Kia <82962111+KiaWeb@users.noreply.github.com>
2022-08-17 17:25:24 -0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -2,9 +2,30 @@
 Official Expo Tools Loader
 Expo in Expo Tools stands for Exploiter.
 Made by Kia.
-Alpha Test 0.1.1a.
+v0.12.1a
 --]]
-getfenv().docs = nil
-getfenv().new = function(type,parent)
-  return Instance.new(type,parent)
+-- Synapse Library
+-- [NF] means not found.
+local env = getgenv()
+env.esyn = function()
+  return "eSyn library. Synapse X supported."
 end
+local log = function(ex)
+  rconsoleprint('@@BLUE@@')
+  rconsoleprint(ex)
+end
+env.esyn.mt = {
+  __index = function(t,i)
+    log(t..", "..i.." indexed")
+  end
+  __newindex = function(t,i,v)
+    log(t..", "..i..", "..v.." index trying to be set")
+  end
+  __call = function(t)
+    log(t.." called")
+  end
+  __concat = function(t,v)
+    log(t..", "..v.." concat")
+  end
+}
+  
